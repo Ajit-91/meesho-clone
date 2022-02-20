b1f2f1e639b7102248191a53b61ca97722251345
Ajit Kumar <ajitkumar12105@gmail.com>
2022-02-20 21:09:09 +0530

    added approoveCatalog

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,7 +4,7 @@ const  isAdmin = require("../middlewares/isAdmin");
 const  isAuthenticated = require("../middlewares/isAuthenticated");
 const router = express.Router()
 
-router.get("/api/admin/approve", isAuthenticated, isAdmin, approveCatalog)
+router.get("/api/admin/approve-catalog/:catalogid", isAuthenticated, isAdmin, approveCatalog)
 router.get("/api/admin/fetch-all-catalogs", isAuthenticated, isAdmin, fetchAllCatalogs)
 
 module.exports = router
